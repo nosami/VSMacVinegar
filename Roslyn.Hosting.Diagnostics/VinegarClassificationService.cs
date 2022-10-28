b33749fb4d4bf10f4ca9dcfbddbc114b1bfa92d7
nosami <jasonimison@gmail.com>
2022-10-28 14:58:51 +0100

    Add Roslyn.Hosting.Diagnostics project

---
+++ Roslyn.Hosting.Diagnostics/VinegarClassificationService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Classification;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.PooledObjects;
+using Microsoft.CodeAnalysis.Text;
+//using Microsoft.VisualStudio.Utilities;
+
+namespace Vinegar
+{
+
+    [ExportLanguageService(typeof(IClassificationService), "vinegar", layer: ServiceLayer.Host)]
+	public class VinegarClassificationService: IClassificationService
+	{
+        public VinegarClassificationService()
+        {
+            string s = null;
+        }
+        private readonly ObjectPool<List<ClassifiedSpan>> s_listPool = new(() => new());
+
+        public ClassifiedSpan AdjustStaleClassification(SourceText text, ClassifiedSpan classifiedSpan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TextChangeRange? ComputeSyntacticChangeRange(SolutionServices workspace, SyntaxNode oldRoot, SyntaxNode newRoot, TimeSpan timeout, CancellationToken cancellationToken)
+        {
+            return null;
+        }
+
+        public ValueTask<TextChangeRange?> ComputeSyntacticChangeRangeAsync(Document oldDocument, Document newDocument, TimeSpan timeout, CancellationToken cancellationToken)
+        {
+            return new();
+        }
+
+        Task IClassificationService.AddEmbeddedLanguageClassificationsAsync(Document document, TextSpan textSpan, ClassificationOptions options, ArrayBuilder<ClassifiedSpan> result, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        void IClassificationService.AddLexicalClassifications(SourceText text, TextSpan textSpan, ArrayBuilder<ClassifiedSpan> result, CancellationToken cancellationToken)
+        {
+            using var _ = s_listPool.GetPooledObject(out var list);
+            if (text.Lines.GetLineFromPosition(textSpan.Start).LineNumber % 2 == 0)
+            {
+                result.Add(new ClassifiedSpan(textSpan, "class"));
+            }
+            else
+            {
+                result.Add(new ClassifiedSpan(textSpan, "text"));
+            }
+        }
+
+        Task IClassificationService.AddSemanticClassificationsAsync(Document document, TextSpan textSpan, ClassificationOptions options, ArrayBuilder<ClassifiedSpan> result, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        void IClassificationService.AddSyntacticClassifications(SolutionServices services, SyntaxNode root, TextSpan textSpan, ArrayBuilder<ClassifiedSpan> result, CancellationToken cancellationToken)
+        {
+            
+            string s = null;
+        }
+
+        Task IClassificationService.AddSyntacticClassificationsAsync(Document document, TextSpan textSpan, ArrayBuilder<ClassifiedSpan> result, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Roslyn.Hosting.Diagnostics/VinegarLanguageService.cs
@@ -0,0 +1,48 @@
+﻿using System.Composition;
+using Microsoft.CodeAnalysis.Editor;
+
+using Microsoft.VisualStudio.Utilities;
+using ExportAttribute = System.ComponentModel.Composition.ExportAttribute;
+    
+namespace Vinegar
+{
+
+    public static class VinegarContentTypeDefinitions
+    {
+        static VinegarContentTypeDefinitions()
+        {
+            var s = "hello";
+            s = "there;";
+        }
+        [Export]
+        [FileExtension(".vinegar")]
+        [Name(ContentTypeNames.VinegarContentType)]
+        [BaseDefinition(ContentTypeNames.RoslynContentType)]
+        [BaseDefinition("text")]
+        public static readonly ContentTypeDefinition VinegarContentTypeDefinition;
+    }
+
+    public static class ContentTypeNames
+    {
+        public const string RoslynContentType = "Roslyn Languages";// Microsoft.CodeAnalysis.Editor.ContentTypeNames.RoslynContentType;
+        public const string VinegarContentType = "vinegar";
+    }
+
+    [ExportContentTypeLanguageService(ContentTypeNames.VinegarContentType, ContentTypeNames.VinegarContentType), Shared]
+    class VinegerContentTypeLanguageService : IContentTypeLanguageService
+    {
+        private readonly IContentTypeRegistryService _contentTypeRegistry;
+
+        [ImportingConstructor]
+        //[Obsolete(MefConstruction.ImportingConstructorMessage, error: true)]
+        public VinegerContentTypeLanguageService(IContentTypeRegistryService contentTypeRegistry)
+        {
+            _contentTypeRegistry = contentTypeRegistry;
+        }
+
+        public IContentType GetDefaultContentType()
+        {
+            return _contentTypeRegistry.GetContentType(ContentTypeNames.VinegarContentType);
+        }
+    }
+}
