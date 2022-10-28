5a22d8350615b222dc3dc52c46268e89a56e5002
nosami <jasonimison@gmail.com>
2022-10-28 15:10:39 +0100

    Move project files

---
+++ Vinegar/Properties/AddinInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Xml.Linq;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+
+[assembly: Addin(
+"Vinegar",
+    Namespace = "Vinegar",
+    Version = VersionInfo.Version
+)]
+
+[assembly: AddinName("Vinegar")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinUrl("https://whatever")] //
+[assembly: AddinDescription("Vim Vinegar clone for VSMac")]
+[assembly: AddinAuthor("Jason Imison")]
+
+public static class VersionInfo
+{ 
+    public const string Version = "0.0.0.7";
+}
